c5de28b1539a1e0e361cd2e8def91dc50eb4cecc
Marwanmhks <66640498+Marwanmhks@users.noreply.github.com>
2022-03-22 17:16:05 +0200

    Update path_planner.py

--- src/path_planner.py
+++ src/path_planner.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python3
 import rospy
+import tf
+import tf2_ros
+import sys
 
 from std_msgs.msg import Header
 from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
@@ -22,6 +25,8 @@ UNKNOWN_CONE_STYLE = rospy.get_param("planner/unknown_cone_style", 4)
 rospy.init_node('path_planner')
 
 waypoints_pub = rospy.Publisher(WAYPOINTS_TOPIC, navPath, queue_size=1)
+broadcaster = tf2_ros.StaticTransformBroadcaster()
+static_transformStamped = nav_msgs.msg.TransformStamped()
 
 while not rospy.is_shutdown():
     
@@ -37,6 +42,10 @@ while not rospy.is_shutdown():
         output_path.header.frame_id = 'map'
         waypoints_pub.publish(output_path)
     else:
-        output_path.header.frame_id = 'map'
+        static_transformStamped.header.stamp = rospy.Time.now()
+        static_transformStamped.header.frame_id = "map"
+        static_transformStamped.child_frame_id = sys.argv[1]
         output_path.poses = [PoseStamped(Header(frame_id = 'map'), Pose(Point(x = waypoint.x, y = waypoint.y), Quaternion())) for waypoint in best_path.waypoints]
+        static_transformStamped.transform.poses = output_path.poses
+        broadcaster.sendTransform(static_transformStamped)
         waypoints_pub.publish(output_path)
